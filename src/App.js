3df2d7313d4a30ce3b602de5794b0360ff97dd3c
Anneke <122925139+anneke44@users.noreply.github.com>
2025-07-21 16:56:50 +0200

    Added new component for weather search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
+        <SearchBar />
       </header>
     </div>
   );
---
+++ src/SearchBar.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchBar() {
+  const [input, setInput] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function searchWeather(response) {
+    setLoaded(true);
+    setWeather({
+      city: response.data.city,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "04f3tf2c9f9bboc83b5050dcf54e2f1a";
+    let apiQuery = input;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${apiQuery}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(searchWeather);
+  }
+
+  function handleInput(event) {
+    setInput(event.target.value);
+  }
+
+  let form = (
+    <div className="SearchBar mt-4">
+      <form action="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter City"
+          autoComplete="off"
+          onChange={handleInput}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <div>
+          <div> {weather.city} </div>
+          <div> {Math.round(weather.temperature)}°C </div>
+          <div> {Math.round(weather.wind)}km/h </div>
+          <div> {weather.humidity}% </div>
+          <div> {weather.description} </div>
+          <div>
+            <img src={weather.icon} alt={weather.description} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
